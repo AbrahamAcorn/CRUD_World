3fbdd821a834367f89ff42551c6e98bd80476bfb
Abraham <abrahmr0@gmail.com>
2021-05-15 21:25:40 -0500

    Cambios en la plantilla utilizada

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from flaskext.mysql import MySQL
 
 app=Flask(__name__)
 
+@app.route("/")
+def index():
+
+    return render_template('/landing-page.html')
+
 mysql= MySQL()
 app.config['MYSQL_DATABASE_HOST']='localhost'
 app.config['MYSQL_DATABASE_USER']='root'
@@ -13,8 +18,8 @@ app.config['MYSQL_DATABASE_PASSWORD']=''
 app.config['MYSQL_DATABASE_DB']='world'
 mysql.init_app(app)
 
-@app.route('/')
-def index():
+@app.route('/city')
+def maincity():
     sql="SELECT * FROM `city`;"
     conn=mysql.connect()
     cursor=conn.cursor()
